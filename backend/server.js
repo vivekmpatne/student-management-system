0386b6e65207ba846057676e757283b9ddb7638f
vivekmpatne <vivekpatnem@gmail.com>
2026-05-09 21:02:16 +0530

    Deployment setup

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ app.get("/", (req, res) => {
 // -------------------------------------------------------
 // Start Server
 // -------------------------------------------------------
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
